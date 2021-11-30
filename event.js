2e20b68fe07a52e6589da2eb9cb3b6a0c13d7c0d
chaseschlachter <45405981+chaseschlachter@users.noreply.github.com>
2021-11-30 17:04:48 -0600

    Create event.js

---
+++ event.js
@@ -0,0 +1,37 @@
+// here is the structure of the mongodb model
+// keep in mind the guests are in a nested array
+
+const eventSchema = new Schema({
+  event_name: String,
+  venue_name: String,
+  address: String,
+  description: String,
+  location: String,
+  artist: {
+    type: Schema.Types.ObjectId,
+    ref: 'Artist'
+  },
+  guests: [
+    {
+      phone: Number,
+      attended: String // this will be the "yes" or "no" from the prompt page
+      //ref: 'Guest'
+    }
+  ],
+  created: {
+    type: Date,  // Captures both date and time
+    default: Date.now
+  },
+  event_start: {
+    type: Date,
+    required: [true, 'Date & time of event start required']
+    },
+  event_end: {
+    type: Date,
+    required: [true, 'Date & time of event end required']
+  },
+  notification: {
+    type: String,
+    default: 30
+  },
+}, opts);
